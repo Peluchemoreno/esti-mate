a5f19a3449decc736d85a55a481340a226e64075
Justin McDonald <jmcdmoreno19@gmail.com>
2024-09-19 16:32:38 -0500

    update routing for each project (temporary)

--- src/components/ProjectRowData/ProjectRowData.jsx
+++ src/components/ProjectRowData/ProjectRowData.jsx
@@ -1,4 +1,5 @@
 import "./ProjectRowData.css";
+import { Link } from "react-router-dom";
 
 export default function ProjectRowData({projectName, owner, created}) {
   return (
@@ -7,7 +8,7 @@ export default function ProjectRowData({projectName, owner, created}) {
         <label className="project__select-checkbox checkbox">
           <input type="checkbox" name="select-project" id="selectProject" />
         </label>
-        <span>{projectName}</span>
+        <span><Link className="project__link" to={projectName} >{projectName}</Link></span>
       </td>
       <td className="project__table-owner-column">{owner}</td>
       <td className="project__table-created-column">{created}</td>
