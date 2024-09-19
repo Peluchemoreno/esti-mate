da1600c3d6fa248a7d55b622fc973fd142e2d0d5
Justin McDonald <jmcdmoreno19@gmail.com>
2024-09-19 17:29:57 -0500

    investigate navigation

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,14 @@
 import './PageNotFound.css'
+import { useNavigation } from 'react-router-dom'
 
 export default function PageNotFound(){
+
+  const navigation = useNavigation()
+  console.log(navigation)
+
   return (
     <div className="page-not-found">
-      this page is not found
+      this page is not found {navigation}
     </div>
   )
 }
