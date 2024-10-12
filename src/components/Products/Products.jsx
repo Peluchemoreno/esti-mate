09bc4798965a171912a678d32ff8ee373d47e61f
Justin McDonald <jmcdmoreno19@gmail.com>
2024-10-11 21:58:00 -0500

    cleanup css

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,5 @@
 import "./Products.css";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, gridClasses, GridToolbar } from "@mui/x-data-grid";
 import { Box } from "@mui/material/";
 import { useState } from "react";
 
@@ -7,12 +7,12 @@ import { useState } from "react";
 export default function Products() {
   const [tableData, setTableData] = useState({
     rows: [
-      { id: 1, col1: `6" K-Style`, col2: {} , col3: "Length/Feet", col4: '$12.15'},
-      { id: 2, col1: `5" Half Round`, col2: {} , col3: "Length/Feet", col4: '$24.10' },
-      { id: 3, col1: "Euro Box", col2: {} , col3: "Length/Feet", col4: '$24.15' },
-      { id: 4, col1: `5" K-Style`, col2: {} , col3: "Length/Feet", col4: '$10.15' },
-      { id: 5, col1: `5" Straight Face`, col2: {} , col3: "Length/Feet", col4: '$15.15' },
-      { id: 6, col1: `6" Straight Face`, col2: {} , col3: "Length/Feet", col4: '$18.05' },
+      { id: 1, col1: `6" K-Style`, col2: 'red' , col3: "Length/Feet", col4: '$12.15'},
+      { id: 2, col1: `5" Half Round`, col2: 'orange' , col3: "Length/Feet", col4: '$24.10' },
+      { id: 3, col1: "Euro Box", col2: 'yellow' , col3: "Length/Feet", col4: '$24.15' },
+      { id: 4, col1: `5" K-Style`, col2: 'green' , col3: "Length/Feet", col4: '$10.15' },
+      { id: 5, col1: `5" Straight Face`, col2: 'blue' , col3: "Length/Feet", col4: '$15.15' },
+      { id: 6, col1: `6" Straight Face`, col2: 'magenta' , col3: "Length/Feet", col4: '$18.05' },
     ],
     columns: [
       {
@@ -58,11 +58,61 @@ export default function Products() {
             bgcolor: "transparent",
           },
           fontWeight: "bold",
+          [`.${gridClasses.cell}.red`]: {
+            backgroundColor: 'red',
+            color: 'white',
+            fontWeight: 'bold'
+          },
+          [`.${gridClasses.cell}.orange`]: {
+            backgroundColor: 'orange',
+            color: 'white',
+            fontWeight: 'bold'
+          },
+          [`.${gridClasses.cell}.yellow`]: {
+            backgroundColor: 'yellow',
+            color: 'black',
+            fontWeight: 'bold'
+          },
+          [`.${gridClasses.cell}.green`]: {
+            backgroundColor: 'green',
+            color: 'black',
+            fontWeight: 'bold'
+          },
+          [`.${gridClasses.cell}.blue`]: {
+            backgroundColor: 'blue',
+            color: 'white',
+            fontWeight: 'bold'
+          },
+          [`.${gridClasses.cell}.magenta`]: {
+            backgroundColor: 'magenta',
+            color: 'white',
+            fontWeight: 'bold'
+          },
         }}
       >
         <DataGrid
           rows={tableData.rows}
           columns={tableData.columns}
+          
+          getCellClassName={(params)=>{
+            if (params.field === 'col1' || params.value == null){
+              return ''
+            }
+            switch (params.value){
+              case 'red':
+                return 'red'
+              case 'orange':
+                return 'orange'
+              case 'yellow':
+                return 'yellow'
+              case 'green':
+                return 'green'
+              case 'blue':
+                return 'blue'
+              case 'magenta':
+                return 'magenta'
+            }
+          }}
           sx={{
             color: "white",
           }}
